8e368a569394e45d67d0cef6e9496757b90ec745
Sebastian Barrios <20190222@aloe.ulima.edu.pe>
2022-11-19 21:02:25 -0500

    agregado del backend

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
-const { Confederacion, Grupo, Equipo } = require("./dao")
+const { Orden, Orden_producto, PC_Armado, PC_Armado_Prod,
+    Producto, Reporte, Resena, Usuario } = require("./dao")
 const PUERTO = 4444
 const app = express()
 app.use(bodyParser.json())
@@ -10,3 +11,41 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(cors())
 app.use(express.static("assets"))
+
+app.get("/Orden", async (req, resp) => {
+    const listaOrden = await Orden.findAll()
+    resp.send(listaOrden)
+})
+app.get("/Orden_producto", async (req, resp) => {
+    const listaOrdenProd = await Orden_producto.findAll()
+    resp.send(listaOrdenProd)
+})
+app.get("/PC_Armado", async (req, resp) => {
+    const listaPCArmado = await PC_Armado.findAll()
+    resp.send(listaPCArmado)
+})
+app.get("/PC_Armado_Prod", async (req, resp) => {
+    const listaPCArmadoProd = await PC_Armado_Prod.findAll()
+    resp.send(listaPCArmadoProd)
+})
+app.get("/Producto", async (req, resp) => {
+    const listaProducto = await Producto.findAll()
+    resp.send(listaProducto)
+})
+app.get("/Reporte", async (req, resp) => {
+    const listaReporte = await Reporte.findAll()
+    resp.send(listaReporte)
+})
+app.get("/Resena", async (req, resp) => {
+    const listaResena = await Resena.findAll()
+    resp.send(listaResena)
+})
+app.get("/Usuario", async (req, resp) => {
+    const listaUsuario = await Usuario.findAll()
+    resp.send(listaUsuario)
+})
+
+
+app.listen(PUERTO, () => {
+    console.log(`Servidor web iniciado en el puero ${PUERTO}`)
+})
