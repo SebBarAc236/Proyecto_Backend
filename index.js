ea1a94fc2ccde82190ba1ad9abd7851d33b8f2cd
Jannire Trujillo <Janniretruji@gmail.com>
2022-12-01 01:03:32 -0500

    a2

--- index.js
+++ index.js
@@ -10,11 +10,22 @@ const TOKEN = "HSDFOSHFHSDFSDHFJSHK"
 const usuarioID = ""
 const ERRORLOGIN = "Datos incorrectos"
 
+var whitelist = ['https://proyecto-frontend-production.up.railway.app']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended : true
 }))
-app.use(cors())
+
 app.use(express.static("assets"))
 
 app.get("/Orden", async (req, resp) => {
